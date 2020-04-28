ec5abea32c7822cc425d945ead3f3af639ef96c4
DEOK <ziinxed@gmail.com>
2020-04-29 00:06:07 +0900

    first migrate crawl

---
+++ crawl/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+SOURCE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted([(item, item) for item in get_all_styles()])
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    url = models.TextField()
+    language = models.CharField(choices=SOURCE_CHOICES, default='python', max_length=100)
+    class Meta:
+        ordering = ['created']
+
+# Create your models here.
